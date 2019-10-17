2aafb935278bfaf9a5c25022c359a64e8fb0020f
muklasr <muclasrachmanto@gmail.com>
2019-10-17 09:03:36 +0700

    first commit

---
+++ app/src/main/java/com/mr/sinaustorage/MainActivity.java
@@ -0,0 +1,98 @@
+package com.mr.sinaustorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etContent;
+    private TextView tvContent;
+    public static final String FILENAME = "example.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etContent = findViewById(R.id.et_content);
+        tvContent = findViewById(R.id.tv_content);
+    }
+
+    public void createFile(View view) {
+        String content = etContent.getText().toString();
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream fileOutputStream;
+        try {
+            file.createNewFile();
+            fileOutputStream = new FileOutputStream(file, true);
+            fileOutputStream.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void editFile(View view){
+        String content = etContent.getText().toString();
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream fileOutputStream;
+        try {
+            file.createNewFile();
+            fileOutputStream = new FileOutputStream(file, false);
+            fileOutputStream.write(content.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(View view){
+        File file = new File(getFilesDir(), FILENAME);
+        if(file.exists()){
+            StringBuilder text = new StringBuilder();
+
+            try{
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+                String line = bufferedReader.readLine();
+
+                while (line != null){
+                    text.append(line);
+                    line = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            tvContent.setText(text);
+        } else {
+            tvContent.setText("File not exist");
+        }
+    }
+
+    public void deleteFile(View view){
+        File file = new File(getFilesDir(), FILENAME);
+        if(file.exists()){
+            file.delete();
+        } else {
+            tvContent.setText("File not exist");
+        }
+    }
+}
